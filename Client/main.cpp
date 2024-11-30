ec58285a86308dcae7b748c6f6af112c7f0d2544
Dima Hubatenko <gubatenkod@gmail.com>
2024-11-30 20:47:08 +0200

    Started implementing intercept lib to run app by using withdll.exe

--- Client/main.cpp
+++ Client/main.cpp
@@ -13,28 +13,28 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    // QQuickStyle::setStyle("Fusion");
+    QQuickStyle::setStyle("Fusion");
 
-    // qmlRegisterType<FileSystemDataSource>("FileSystemModel", 1, 0, "FileSystemDataSource");
-    // qmlRegisterType<Server>("Server", 1, 0, "Server");
+    qmlRegisterType<FileSystemDataSource>("FileSystemModel", 1, 0, "FileSystemDataSource");
+    qmlRegisterType<Server>("ServerModule", 1, 0, "Server");
 
-    // QQmlApplicationEngine engine;
-    // QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() {
-    //     QCoreApplication::exit(-1);
-    // }, Qt::QueuedConnection);
+    QQmlApplicationEngine engine;
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() {
+        QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
 
-    // engine.loadFromModule("qml.module", "MainWindow");
+    engine.loadFromModule("qml.module", "MainWindow");
 
-    Connection_pool* pool = Connection_pool::init(nullptr, "192.168.0.14:8091");
+    // Connection_pool* pool = Connection_pool::init(nullptr, "192.168.0.14:8091");
 
-    Filesystem::get_instance()->cache_folder = "F:/Server";
-    Filesystem::get_instance()->mount_path = "Y:";
-    Filesystem::get_instance()->start();
+    // Filesystem::get_instance()->cache_folder = "F:/Server";
+    // Filesystem::get_instance()->mount_path = "Y:";
+    // Filesystem::get_instance()->start();
 
-    auto res = app.exec();
+    // auto res = app.exec();
 
-    Filesystem::get_instance()->terminate();
-    pool->deleteLater();
+    // Filesystem::get_instance()->terminate();
+    // pool->deleteLater();
 
-    return res;
+    return app.exec();
 }
